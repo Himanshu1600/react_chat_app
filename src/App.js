c5e33055aec11e80f7ef6b9fd76a5a963185646c
Himanshu1600 <himanshuparihar1600@gmail.com>
2021-08-10 17:25:11 +0530

    react chat app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from "./ChatFeed";
+import Loginform from "./Loginform";
 function App() {
+  if (!localStorage.getItem("username")) return <Loginform />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ChatEngine
+        height="100vh"
+        projectID="acafeeb7-f742-47a7-ac2e-b461920c9128"
+        userName={localStorage.getItem("username")}
+        userSecret={localStorage.getItem("password")}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() => new Audio("./sentsound.mp3").play()}
+      />
+    </>
   );
 }
 
